e2826e0da673a45a270a174aa67198a9eb37962a
Mchllar <mchllatieno@gmail.com>
2023-11-29 21:28:33 +0300

    Third Deliverable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,7 +104,6 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
-    implementation ("org.tensorflow:tensorflow-lite:2.7.0")
 
 
 
